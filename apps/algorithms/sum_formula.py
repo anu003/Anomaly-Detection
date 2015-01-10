616e87090a40741403a38b8c7a6e5dc2fad0fb87
Cenk <cenk.math@gmail.com>
2015-01-10 13:12:05 +0200

    standart deviation and variance calculation added

---
+++ apps/algorithms/sum_formula.py
@@ -0,0 +1,28 @@
+import math
+
+__author__ = 'cenk'
+
+
+class SumFormula:
+    def __init__(self):
+        self.data = []
+
+    def exponential(self, x, power):
+        return math.pow(x, power)
+
+    def calculate(self, data, is_tuple=False, index=None, power=1):
+        if is_tuple:
+            self.data = [obj[index] for obj in data]
+        else:
+            self.data = data
+        self.power = power
+        return self.__algorithm()
+
+    def __algorithm(self):
+        total = 0.0
+        for num in self.data:
+            total += self.exponential(num, self.power)
+        try:
+            return total
+        except:
+            raise
